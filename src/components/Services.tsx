d9eec226915e0a263fd5c74eff4f2ad00709c227
Resheph Inayat <rasafinayat@gmail.com>
2025-01-07 21:27:44 +0500

    Change

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,7 @@ function Services() {
   return (
     <main className="w-full h-full">
       <h1 className="font-bold text-4xl text-center ">
-        We are In The Business Of Helping You Grow Your Business
+        We are In The Business Of Helping You Grow Your Business.
       </h1>
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
